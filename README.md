7fdd63d9c0155c0fe98de769c6c849c85b95319b
Ross Golder <ross@golder.org>
2017-03-16 08:49:36 +0700

    Initial check-in.

---
+++ README.md
@@ -0,0 +1,13 @@
+# node-red-contrib-hipchat-v2
+
+  A simple node to pass a HipChat message via API v2.
+
+## Installation
+
+```bash
+$ npm install node-red-contrib-hipchat-v2
+```
+
+## Usage
+
+Add the node to your flow, set the API key and room id. The node will pass the payload on input to the API.
---
+++ hipchat-v2.js
@@ -0,0 +1,20 @@
+module.exports = function(RED) {
+    "use strict";
+    var hipchatter = require("hipchatter");
+
+    function HipchatV2Node(config) {
+        RED.nodes.createNode(this,config);
+        var node = this;
+
+        var notify_error = function(err) {
+          if(err != null) node.error(err);
+        };
+
+        var hc = new hipchatter(config.apikey);
+
+        this.on('input', function(msg) {
+          hc.notify(config.roomid, msg.payload, notify_error);
+        });
+    }
+    RED.nodes.registerType("hipchat-v2",HipchatV2Node,{});
+}
